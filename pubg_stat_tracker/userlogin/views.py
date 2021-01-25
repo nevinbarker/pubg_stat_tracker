7efb3aad5495c40bd4f030cf273613ceb8941f32
Nevin Barker <mrwafflesman@gmail.com>
2021-01-24 23:24:12 -0600

    1/24

--- pubg_stat_tracker/userlogin/views.py
+++ pubg_stat_tracker/userlogin/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Enter your username and password.")
+    template = loader.get_template('userlogin/index.html')
+    context = {
+        'username': 'password'
+    }
+    return HttpResponse(template.render(context, request))
 
