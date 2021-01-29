72203467bc49484804068372a1d12eb58a5d654c
Nevin Barker <mrwafflesman@gmail.com>
2021-01-28 19:47:19 -0600

    added bootstrap and base html

--- pubg_stat_tracker/pubg_stat_tracker/settings.py
+++ pubg_stat_tracker/pubg_stat_tracker/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'homepage.apps.HomepageConfig',
     'userlogin.apps.UserloginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
