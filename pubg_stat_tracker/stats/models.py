024fffe10e206ff443732c03ef1f667d17314b03
Nevin Barker <mrwafflesman@gmail.com>
2021-02-14 21:52:27 -0600

    continued work on PUBG API call

--- pubg_stat_tracker/stats/models.py
+++ pubg_stat_tracker/stats/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Stats(models.Model):
 
     matchId = models.IntegerField()
-    kills = models.IntegerFieldField()
-    deaths = models.IntegerFieldField()
-    damage = models.IntegerFieldField()
-    dbno = models.IntegerFieldField()
-    revives = models.IntegerFieldField()
-    timeAlive = models.IntegerFieldField()
+    kills = models.IntegerField()
+    deaths = models.IntegerField()
+    damage = models.IntegerField()
+    dbno = models.IntegerField()
+    revives = models.IntegerField()
+    timeAlive = models.IntegerField()
     user = models.ForeignKey('userlogin.Profile', on_delete=models.CASCADE)
 
--- pubg_stat_tracker/stats/views.py
+++ pubg_stat_tracker/stats/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 import requests
 from django.views.generic.list import ListView
 from .models import Stats
+from django.contrib import messages
 
 
 @login_required
@@ -11,23 +12,38 @@ def stats(request):
         "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJmNGFiYTExMC00YjIyLTAxMzktZmU3YS0wZDExNzBkZGYxZjMiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjEyNjY4NzE3LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1Ymdfc3RhdF90cmFjIn0.sKeF4EzTsxy714VEaWY5n19j35vsJvH15gTzBb7cmH0",
         "Accept": "application/vnd.api+json"
     }
-    playerName = 'mrwafflesman'
-    response = requests.get(f'https://api.pubg.com/shards/steam/players?filter[playerNames]={playerName}', headers=header)
+    pubgUsername = request.user.profile.pubgUsername
 
-    context = response.json()
-
-    matches = context['data'][0]['relationships']['matches']['data']
-
-    if len(matches) < 1:
-        print('no matches')
+    if pubgUsername == '':
+        messages.warning(request, "You haven't linked your PUBG account. Go to the profile page to link your account.")
     else:
-        for match in matches:
-            matchId = match['id']
-            response = requests.get(f'https://api.pubg.com/shards/steam/matches/{matchId}')
-            r = response.json()
+        response = requests.get(f'https://api.pubg.com/shards/steam/players?filter[playerNames]={pubgUsername}',
+                                headers=header)
+
+        context = response.json()
+
+        if 'errors' in context:
+            messages.warning(request, "There is no PUBG account with that username. make sure your PUBG username is correct in your profile page.")
+        else:
+            matches = context['data'][0]['relationships']['matches']['data']
+
+            if len(matches) < 1:
+                pass
+            else:
+                for match in matches:
+                    matchId = match['id']
+
+                    if Stats.objects.filter(matchId=matchId).exists():
+                        pass
+                    else:
+                        response = requests.get(f'https://api.pubg.com/shards/steam/matches/{matchId}')
+                        r = response.json()
+                        Stats.objects.create(
+                            matchId=matchId,
+                        )
 
     return render(request, 'stats/stats.html', {
-        'playerName': context['data'][0]['attributes']['name']
+        'playerName': pubgUsername
     })
 
 
